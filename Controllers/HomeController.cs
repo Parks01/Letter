f2ae05d315ab874a6c5841d7137ee009f9d545c3
Andrew Parks and Michael Woldemedihin <andrew@>
2017-07-31 16:28:25 -0700

    add a form.cshtml, Form action method, GreetingCard action method

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using FriendLetter.Models;
 
 namespace FriendLetter.Controllers
 {
@@ -10,17 +11,34 @@ namespace FriendLetter.Controllers
       public string Hi() { return "Hello Dude"; }
 
       public string Goodbye() { return "Goodbye friend!"; }
-
+      [Route("/favorite_photos")]
+      public ActionResult FavoritePhotos()
+      {
+        return View();
+      }
       [Route("/hello")]
       public ActionResult Hello()
+      {
+        LetterVariable myLetterVariable = new LetterVariable();
+        myLetterVariable.SetRecipient("Erin");
+        myLetterVariable.SetSender("Mike");
+
+        return View(myLetterVariable);
+      }
+
+      [Route("/form")]
+      public ActionResult Form()
       {
         return View();
       }
 
-      [Route("/favorite_photos")]
-        public ActionResult FavoritePhotos()
-        {
-          return View();
-        }
+      [Route("/greeting_card")]
+      public ActionResult GreetingCard()
+      {
+          LetterVariable myLetterVariable = new LetterVariable();
+          myLetterVariable.SetRecipient(Request.Query["recipient"]);
+          myLetterVariable.SetSender(Request.Query["sender"]);
+          return View("Hello", myLetterVariable);
+      }
     }
 }
