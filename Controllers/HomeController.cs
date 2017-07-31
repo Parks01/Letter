aa819117ca626ef364ffc1aec8122fd9b86e0dc1
Andrew Parks and Michael Woldemedihin <michael@>
2017-07-31 15:07:37 -0700

    add a separate view hello.cshtml

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -11,52 +11,16 @@ namespace FriendLetter.Controllers
 
       public string Goodbye() { return "Goodbye friend!"; }
 
-      [Produces("text/html")]
-      [Route("/")]
-      public string Letter()
+      [Route("/hello")]
+      public ActionResult Hello()
       {
-        return
-        "<!DOCTYPE html>" +
-        "<html>" +
-        "<head>" +
-          "<title>Hello Friend!</title>" +
-          "<link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css'>" +
-        "</head>" +
-        "<body>" +
-          "<h1>Hello From Afar</h1>" +
-          "<p>Dear Friend,</p>" +
-          "<p>How are you? I hope that you are having a nice weekend. I'm learning programming while in Iceland! </p>" +
-          "<p>Friend, you would not believe how cold it is here. I should have gone to Hawaii instead.</p>" +
-          "<p>But I like programming a lot, so I've got that going for me. </p>" +
-          "<p>Looking forward to seeing you soon. I'll bring you back a souvenir. </p>" +
-          "<p>Cheers,</p>" +
-          "<p>Travel Enthusiast Jane</p>" +
-          "<li><img src='/img/white-fox.jpg'/></li>" +
-          "<li><img src='/img/fox-pic.jpg'/></li>" +
-        "</body>" +
-        "</html>" ;
-
-
+        return View();
       }
-      [Produces("text/html")]
+
       [Route("/favorite_photos")]
-      public string FavoritePhotos()
-      {
-      return
-        "<!DOCTYPE html>" +
-        "<html>" +
-          "<head>" +
-            "<title>Hello Friend!</title>" +
-            "<link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css'>" +
-          "</head>" +
-          "<body>" +
-            "<h1>Favorite Traveling Photos</h1>" +
-            "<ul>" +
-              "<li><img src='/img/white-fox.jpg'/></li>" +
-              "<li><img src='/img/fox-pic.jpg'/></li>" +
-            "</ul>" +
-          "</body>" +
-        "</html>";
-    }
+        public ActionResult FavoritePhotos()
+        {
+          return View();
+        }
     }
 }
